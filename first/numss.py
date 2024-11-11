3edb41c0e95a00244089cf01a67163ce1de42a74
danil <danilasoloviev.moscow@yandex.ru>
2024-11-11 03:47:53 +0300

    Добавлена функция умножения, добавлен модуль strings.py, удален текст

--- first/numss.py
+++ first/numss.py
@@ -1,10 +1,10 @@
 def plus(a,b):
     return a+b
-
-text ="new text"
-
 def minus(a,b):
     return a - b
+def multi(a,b):
+    return a*b
 if __name__ == '__main__':
     print(plus(2,2))
-    print(minus(2, 2))
+    print(minus(6, 14))
+    print(multi(67, 2))
